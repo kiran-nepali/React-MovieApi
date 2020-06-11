7c9402e984ad458c6347498638de58f8028e06ed
kiran-nepali <cuziamnepali@hotmail.com>
2020-06-11 13:32:25 +0100

    Updates

---
+++ precache-manifest.3dfc70c7666882825c409edc21325479.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de89208d5b0acd76d1950cc4d3ab49c1",
+    "url": "/React-MovieApi/index.html"
+  },
+  {
+    "revision": "2a952d042ec74ec5e880",
+    "url": "/React-MovieApi/static/css/main.f6fc2613.chunk.css"
+  },
+  {
+    "revision": "10219ab6db5f7bf64fc5",
+    "url": "/React-MovieApi/static/js/2.5315d5e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-MovieApi/static/js/2.5315d5e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a952d042ec74ec5e880",
+    "url": "/React-MovieApi/static/js/main.ac5c6e0a.chunk.js"
+  },
+  {
+    "revision": "f7b50db6d281acd9812a",
+    "url": "/React-MovieApi/static/js/runtime-main.f9829606.js"
+  }
+]);
